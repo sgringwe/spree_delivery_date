3bc9790e94477fd20181603f8fb1eb0bb97e503f
Scott Ringwelski <sgringwe@mtu.edu>
2013-04-15 00:41:59 -0400

    Renamed method

--- app/models/order_decorator.rb
+++ app/models/order_decorator.rb
@@ -2,7 +2,7 @@ Spree::Order.class_eval do
   attr_accessible :delivery_date
 
   validate :delivery_date, :presence => true, :allow_nil => false
-  validate :delivery_date_cannot_be_in_the_past
+  validate :delivery_date_must_be_past_today
 
   def delivery_date_must_be_past_today
     if !delivery_date.blank? and !(delivery_date > Date.today)
