d522a4e01e18a446149d41de3cd01a98576efeac
Scott Ringwelski <sgringwe@mtu.edu>
2013-04-15 00:14:51 -0400

    Deface changes to hopefully fix issue

--- app/overrides/delivery_date.rb
+++ app/overrides/delivery_date.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/checkout/delivery",
+Deface::Override.new(:virtual_path => "spree/checkout/_delivery",
                      :name => "add_delivery_date_to_delivery",
                      :insert_after => "[data-hook='shipping_method_inner']",
                      :partial => "spree/checkout/delivery_date",
--- app/overrides/delivery_date_details.rb
+++ app/overrides/delivery_date_details.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/shared/order_details",
+Deface::Override.new(:virtual_path => "spree/shared/_order_details",
                      :name => "add_delivery_date_details_to_order",
                      :insert_before => "[data-hook='order_details']",
                      :partial => "spree/orders/order_delivery_date_details",
