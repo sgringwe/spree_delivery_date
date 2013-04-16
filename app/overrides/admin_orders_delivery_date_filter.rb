ddd975d93b224b7524af295d3d92fb67aac14c37
Scott Ringwelski <sgringwe@mtu.edu>
2013-04-15 22:09:40 -0400

    Add delivery date filter to orders/index view

---
+++ app/overrides/admin_orders_delivery_date_filter.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => "spree/admin/orders/index",
+                     :name => "add_delivery_date_filter_to_orders_index",
+                     :insert_after => ".date-range-fields",
+                     :partial => "spree/admin/orders/delivery_date_filter",
+                     :disabled => false)
