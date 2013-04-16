eb4310a12419e41aba6ba9593b952eb410332fa1
Scott Ringwelski <sgringwe@mtu.edu>
2013-04-15 22:45:53 -0400

    Insert filter correctly

--- app/overrides/admin_orders_delivery_date_filter.rb
+++ app/overrides/admin_orders_delivery_date_filter.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/orders/index",
                      :name => "add_delivery_date_filter_to_orders_index",
-                     :insert_after => ".date-range-fields",
+                     :insert_after => ".date-range-filter",
                      :partial => "spree/admin/orders/delivery_date_filter",
                      :disabled => false)
