2dad02406d5dfa76da2b6189013984db4e97dd7d
Scott Ringwelski <sgringwe@mtu.edu>
2013-04-15 22:36:43 -0400

    Added delivery date to orders idnex table

---
+++ app/overrides/admin_orders_delivery_date_column.rb
@@ -0,0 +1,10 @@
+Deface::Override.new(:virtual_path => "spree/admin/orders/index",
+                     :name => "add_delivery_date_table_header",
+                     :insert_top => "[data-hook='admin_orders_index_headers']",
+                     :partial => "spree/admin/orders/delivery_date_header",
+                     :disabled => false)
+Deface::Override.new(:virtual_path => "spree/admin/orders/index",
+                     :name => "add_delivery_date_table_row_cell",
+                     :insert_top => "[data-hook='admin_orders_index_rows']",
+                     :partial => "spree/admin/orders/delivery_date_cell",
+                     :disabled => false)
