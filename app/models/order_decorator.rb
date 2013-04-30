cfc66a74a5ffe22d21fffb7a3a431054bcaed84a
Scott Ringwelski <sgringwe@mtu.edu>
2013-04-30 16:27:16 -0400

    Updated delivery date logic

--- app/models/order_decorator.rb
+++ app/models/order_decorator.rb
@@ -11,18 +11,19 @@ Spree::Order.class_eval do
     if !delivery_date.blank?
       # Check if delivery date is sunday or monday, which are not allowed
       if [0, 1, 7].include?(delivery_date.wday)
-        errors.add(:delivery_date, "the delivery date cannot be a Sunday or Monday.")
+        errors.add(:delivery_date, "cannot be a Sunday or Monday.")
       end
 
       cutoff = Time.now.change(:hour => 16, :minute => 30).in_time_zone("Eastern Time (US & Canada)")
+      puts cuttoff
       if cutoff.past?
         # It is past 4:30. Order must be > Date.tomorrow
         if !(delivery_date > Date.tomorrow)
-          errors.add(:delivery_date, "it is too late for delivery tomorrow. Please specify a date after tomorrow.")
+          errors.add(:delivery_date, ": It is too late for delivery tomorrow. Please specify a date after tomorrow.")
         end
       else
         if !(delivery_date > Date.today)
-          errors.add(:delivery_date, "it is too late for delivery today. Please specify a date tomorrow or later.")
+          errors.add(:delivery_date, ": It is too late for delivery today. Please specify a date tomorrow or later.")
         end
       end
     end
