2c2a1c0c9217771bcebb77b054f4be8fd671b3d0
Scott Ringwelski <sgringwe@mtu.edu>
2013-04-30 15:55:43 -0400

    Updated delivery date logic

--- app/models/order_decorator.rb
+++ app/models/order_decorator.rb
@@ -7,16 +7,14 @@ Spree::Order.class_eval do
   validate :delivery_date_specific_validation
 
   def delivery_date_specific_validation
-    # Ensure that a delivery date is set
+    # Ensure that a delivery date is set. We don't want to run these validations until it is
     if !delivery_date.blank?
       # Check if delivery date is sunday or monday, which are not allowed
       if [0, 1, 7].include?(delivery_date.wday)
         errors.add(:delivery_date, "the delivery date cannot be a Sunday or Monday.")
       end
 
-      cutoff = Time.now.in_time_zone("Eastern Time (US & Canada)")
-      cutoff.hour = 16 # 4pm
-      cutoff.minute = 30 # 4:30pm
+      cutoff = Time.now.change(:hour => 16, :minute => 30).in_time_zone("Eastern Time (US & Canada)")
       if now.past?
         # It is past 4:30. Order must be > Date.tomorrow
         if !(delivery_date > Date.tomorrow)
