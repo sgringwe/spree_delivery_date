f6af6d94843605a3c349f160aad0646cca169332
Scott Ringwelski <sgringwe@mtu.edu>
2013-05-03 15:50:46 -0400

    Only validate if not in cart or address step

--- app/models/order_decorator.rb
+++ app/models/order_decorator.rb
@@ -4,9 +4,11 @@ Spree::Order.class_eval do
   attr_accessible :delivery_date
 
   validate :delivery_date, :presence => true, :allow_nil => false
-  validate :delivery_date_specific_validation, :if => :validate_delivery_date? 
+  validate :delivery_date_specific_validation
 
   def delivery_date_specific_validation
+    error_occurred = false
+
     # Ensure that a delivery date is set. We don't want to run these validations until it is
     if !delivery_date.blank?
       puts '00---------------------------'
@@ -16,6 +18,7 @@ Spree::Order.class_eval do
       # Check if delivery date is sunday or monday, which are not allowed
       if [0, 1, 7].include?(delivery_date.wday)
         errors.add(:delivery_date, "cannot be a Sunday or Monday.")
+        error_occurred = true
       end
 
       cutoff = Time.now.change(:hour => 17, :min => 00).in_time_zone("Eastern Time (US & Canada)")
@@ -24,17 +27,20 @@ Spree::Order.class_eval do
         # It is past 5:00. Order must be > Date.tomorrow
         if !(delivery_date > Date.tomorrow)
           errors.add(:delivery_date, ": It is too late for delivery tomorrow. Please specify a date after tomorrow.")
+          error_occurred = true
         end
       else
         if !(delivery_date > Date.today)
           errors.add(:delivery_date, ": It is too late for delivery today. Please specify a date tomorrow or later.")
+          error_occurred = true
         end
       end
     end
-  end
 
-  # Only validate the delivery date if state is equal to or past shipping
-  def validate_delivery_date?
-    return !(state == 'cart' or state == 'address')
+    if error_occurred
+      puts 'going to state delivery'
+      go_to_state :delivery
+      puts state
+    end
   end
 end
