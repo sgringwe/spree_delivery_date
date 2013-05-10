6a531d20e561170b6346c8d3ffa782d1c2d3b0ea
Scott Ringwelski <sgringwe@mtu.edu>
2013-05-09 23:29:49 -0400

    Logic updates

--- app/models/order_decorator.rb
+++ app/models/order_decorator.rb
@@ -7,46 +7,23 @@ Spree::Order.class_eval do
   validate :delivery_date, :presence => true, :allow_nil => false
   validate :delivery_date_specific_validation
 
+  # Ensure that a delivery date is set. We don't want to run these validations until it is
+  # Only run the delivery date validations if we are on that step or past.
   def delivery_date_specific_validation
-    error_occurred = false
-
-    # Ensure that a delivery date is set. We don't want to run these validations until it is
-    # Only run the delivery date validations if we are on that step or
     if !delivery_date.blank? && ['payment', 'confirm', 'complete'].include?(state)
-      puts '------------------------------'
-      puts 'running validation'
-      puts state
-      puts '------------------------------'
-      # Check if delivery date is sunday or monday, which are not allowed
+      cutoff = Time.zone.now.change(:hour => 17, :min => 00) # Gets 5pm in EST time zone (config.time_zone)
+
       if [0, 1, 7].include?(delivery_date.wday)
-        puts 'error 1'
         errors.add(:delivery_date, "cannot be a Sunday or Monday.")
-        error_occurred = true
       end
 
-      cutoff = Time.now.change(:hour => 17, :min => 00).in_time_zone("Eastern Time (US & Canada)")
-      puts cutoff
-      if cutoff.past?
-        # It is past 5:00. Order must be > Date.tomorrow
-        if !(delivery_date > Date.tomorrow)
-          puts 'error 2'
+      if cutoff.past? && !(delivery_date > Date.tomorrow)
           errors.add(:delivery_date, ": It is too late for delivery tomorrow. Please specify a date after tomorrow.")
-          error_occurred = true
         end
-      else
-        if !(delivery_date > Date.today)
-          puts 'error 3'
+      elsif !cutoff.past? && !(delivery_date > Date.today)
           errors.add(:delivery_date, ": It is too late for delivery today. Please specify a date tomorrow or later.")
-          error_occurred = true
         end
       end
-    end
-
-    if error_occurred
-      puts 'an error did occur'
-      self.delivery_date = nil
-      self.state = 'delivery'
-      save
     end
   end
 end
